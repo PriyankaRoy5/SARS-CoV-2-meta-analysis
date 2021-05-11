787233b0bcc46e361fa10dae0ed3707461e82c7c
Priyanka Roy <40205521+PriyankaRoy5@users.noreply.github.com>
2021-05-11 11:41:58 -0400

    updated core_functions

--- core_functions.py
+++ core_functions.py
@@ -275,7 +275,7 @@ def lfc_dist_plot(chip_lfc, initial_id=None, res_id=None, paired_cols=None, col_
     
     
 #Control distributions
-def control_dist_plot(chip_lfc, control_name, initial_id, res_id, filename, figsize = (6,4)): 
+def control_dist_plot(chip_lfc, control_name, filename, gene_col = 'Gene Symbol', initial_id=None, res_id=None, paired_cols=None, col_sep = '_', figsize = (6,4)): 
     '''
     Inputs: 
     1. chip_lfc: annotated lfc data frame
@@ -287,7 +287,10 @@ def control_dist_plot(chip_lfc, control_name, initial_id, res_id, filename, figs
     Outputs: kde plots of control distributions (initial, resistant)
     
     '''
-    sharex, lfc_pairs = pair_cols(chip_lfc, initial_id= initial_id, res_id=res_id)
+    if not paired_cols:    
+        sharex, lfc_pairs = pair_cols(chip_lfc, initial_id = initial_id, res_id = res_id, sep = col_sep)
+    else:
+        sharex, lfc_pairs = paired_cols
     controls = get_controls(chip_lfc, control_name)
     nrows = len(lfc_pairs)
     
@@ -314,9 +317,9 @@ def control_dist_plot(chip_lfc, control_name, initial_id, res_id, filename, figs
 
         sns.despine()
 
-    filepath = '../../Figures/' + filename + '_controldist.png'
-    print(filepath)
-    fig.savefig(filepath, bbox_inches = 'tight')
+    #filepath = '../../Figures/' + filename + '_controldist.png'
+    #print(filepath)
+    #fig.savefig(filepath, bbox_inches = 'tight')
     
         
 ## Residual functions 
